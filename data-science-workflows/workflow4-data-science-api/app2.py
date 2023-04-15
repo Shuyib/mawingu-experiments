784d25b422472f830c6268e8075ce42d34a0abaf
Shuyib <benmainye@gmail.com>
2023-04-15 12:06:01 +0300

    Edit app2.pyto run easily.

--- data-science-workflows/workflow4-data-science-api/app2.py
+++ data-science-workflows/workflow4-data-science-api/app2.py
@@ -3,29 +3,56 @@ import gradio as gr
 from pycaret.classification import *
 
 # load pickled model
-saved_final_dt = load_model('Final dt Model 26Nov2022')
+saved_final_dt = load_model("Final dt Model 26Nov2022")
 
 
-compare_model_results = pull()
-
-# get model results
+# define predict function
 def predict(model, sepal_length, sepal_width, petal_length, petal_width):
-  
-    df = pd.DataFrame.from_dict({'sepal_length': [sepal_length], 'sepal_width': [sepal_width], 
-                                 'petal_length': [petal_length], 'petal_width': [petal_width]})
-    
-    model_index = list(compare_model_results['Model']).index(model)
-    model = best[model_index]
-    pred = predict_model(model, df, raw_score=True)
-    return {'Iris-setosa': pred['Score_Iris-setosa'][0].astype('float64'), 
-            'Iris-versicolor': pred['Score_Iris-versicolor'][0].astype('float64'),
-            'Iris-virginica' : pred['Score_Iris-virginica'][0].astype('float64')}
+    # create dataframe for prediction
+    df = pd.DataFrame.from_dict(
+        {
+            "sepal_length": [sepal_length],
+            "sepal_width": [sepal_width],
+            "petal_length": [petal_length],
+            "petal_width": [petal_width],
+        }
+    )
 
-# create the user interface
-model = gr.inputs.Dropdown(list(compare_model_results['Model']), label="Model")
-sepal_length = gr.inputs.Slider(minimum=1, maximum=10, default=data['sepal_length'].mean(), label = 'sepal_length')
-sepal_width = gr.inputs.Slider(minimum=1, maximum=10, default=data['sepal_width'].mean(), label = 'sepal_width')
-petal_length = gr.inputs.Slider(minimum=1, maximum=10, default=data['petal_length'].mean(), label = 'petal_length')
-petal_width = gr.inputs.Slider(minimum=1, maximum=10, default=data['petal_width'].mean(), label = 'petal_width')
+    # predict on the new dataset and return prediction
+    pred = predict_model(saved_final_dt, df, raw_score=True)
 
-gr.Interface(predict, [model,sepal_length,sepal_width,petal_length,petal_width], "label", live=True).launch()
+    # return prediction as a dictionary
+    return {
+        "Iris-setosa": pred["Score_Iris-setosa"][0].astype("float64"),
+        "Iris-versicolor": pred["Score_Iris-versicolor"][0].astype("float64"),
+        "Iris-virginica": pred["Score_Iris-virginica"][0].astype("float64"),
+    }
+
+
+# create the user interface using gradio
+# Create example list
+example = [
+    ["5.1", "3.5", "1.4", "0.2"],
+    ["6.4", "3.2", "4.5", "1.5"],
+    ["6.9", "3.1", "5.4", "2.1"],
+]
+label = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+
+# Create UI
+set_int = gr.Interface(
+    predict,
+    title="Iris Flower Prediction",
+    description="This app predicts the type of Iris flower based on the input parameters.",
+    inputs=[
+        gr.inputs.Slider(minimum=1, maximum=10, default=5.1, label="sepal_length"),
+        gr.inputs.Slider(minimum=1, maximum=10, default=3.5, label="sepal_width"),
+        gr.inputs.Slider(minimum=1, maximum=10, default=1.4, label="petal_length"),
+        gr.inputs.Slider(minimum=1, maximum=10, default=0.2, label="petal_width"),
+    ],
+    outputs="label",
+    examples=example,
+    live=True,
+)
+
+# Run the app
+set_int.launch()
